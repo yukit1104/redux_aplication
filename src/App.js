ae3d61bffb9353c861e622ce385134a98e533f2f
yukit1104 <78680935+yukit1104@users.noreply.github.com>
2021-06-11 17:45:39 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+
 function App() {
-  return <div><h1>Hello, World!</h1></div>;
+  return (
+  <React.Fragment>
+    <label htmlFor="bar">bar</label>
+    <input type="text" onChange={() => console.log("I am clicked!")} />
+  </React.Fragment>
+  )
 }
 
 export default App;
